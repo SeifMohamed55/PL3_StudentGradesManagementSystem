56b931bd667d44dcbe5c840b0b53a80434a585b9
SeifMohamed55 <seifelden073@gmail.com>
2024-12-01 14:39:18 +0200

    Initial Project

---
+++ Program.fs
@@ -0,0 +1,26 @@
+﻿open System
+open System.Windows
+open System.Windows.Forms
+
+// Create the main application
+let createForm() =
+    let form = new Form()
+    form.Text <- "My First F# WinForms App"
+    form.ClientSize <- System.Drawing.Size(300, 200)
+
+    let label = new Label()
+    label.Text <- "Hello from F#"
+    label.Location <- System.Drawing.Point(100, 80)
+    form.Controls.Add(label)
+    form
+
+let main() =
+    let form = createForm()
+    try
+        Application.Run(form)
+    with
+        | :? InvalidOperationException -> printfn "Invalid Operation"
+        | (ex:exn) -> printfn "exception occured:s %s" ex.Message
+ 
+ 
+main()
